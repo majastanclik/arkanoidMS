ec956f66213a4e416d45a54b00826b6810b927e4
Geno <majastanclik435@gmail.com>
2023-11-08 17:02:02 +0100

    jezu

---
+++ arcanoid/arkanoid/Assets/script/tekst.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tekst : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
